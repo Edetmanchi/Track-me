19c74740668fe25279d5bf2a41955c6b555a8306
Edet Effiong <greatstory247@gmail.com>
2024-06-28 23:51:23 +0100

    post edit functionality added

--- track-me/src/components/posts/PostPagination.jsx
+++ track-me/src/components/posts/PostPagination.jsx
@@ -1,9 +1,38 @@
-
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
+  
 export default function PostPagination() {
   return (
-    <div>
-
-        efefffefrfr
-    </div>
+    <>
+        <Pagination className='text-slate-600'>
+            <PaginationContent>
+                <PaginationItem>
+                    <PaginationPrevious href="#" />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">1</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">2</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                     <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationNext href="#" />
+                </PaginationItem>
+            </PaginationContent>
+        </Pagination>
+    </>
   )
 }
