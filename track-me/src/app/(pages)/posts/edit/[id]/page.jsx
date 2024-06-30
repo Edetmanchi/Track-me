5e5d963f928d057f5c525cce0878e01fd2b9c6e0
Edet Effiong <greatstory247@gmail.com>
2024-06-30 23:40:56 +0100

    edit form components added

--- track-me/src/app/(pages)/posts/edit/[id]/page.jsx
+++ track-me/src/app/(pages)/posts/edit/[id]/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import * as z from 'zod'
-import { useForm } from 'react-hook-form'
+import { useForm , useFormContext} from 'react-hook-form'
 import {zodResolver} from '@hookform/resolvers/zod'
 import BackButton from "@/components/BackButton"
 import {
@@ -13,6 +13,10 @@ import {
 import { Input} from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { Button } from '@/components/ui/button'
+import posts from '@/data/posts'
+import { resolve } from 'styled-jsx/css'
+
+
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -33,11 +37,45 @@ const formSchema = z.object({
 
 export default function PostEditPage({params}) {
   const post = posts.find((post) =>post.id === params.id)
-  console.log(post)
+  console.log(post);
+
+  // const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm  ({
+    resolver: zodResolver(formSchema),
+    defaultvalues: {
+      title: post?.tittle || "",
+      body: post?.body || "",
+      author: post?.author || "",
+      date: post?.date || "", 
+    }
+  }) 
+
+  const handleSubmit = (data)=>{
+    console.log(date)
+  }
+
+
+
+
   return (
-    <div>
+    <>
       <BackButton text='Back to Post' link='/posts'/>
-      <span>this is the edit button for Post</span>
-    </div>
+      <h2 className="text-2xl mb-3">Edit posts</h2>
+        <form onSubmit={handleSubmit} action="submit" className="space-y-8">
+          <FormField
+            control={form.control}
+            name="tittle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='uppercase text-s font-bold text-zinc-500 dark:text-secondary/70'>tittle</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+    </>
   )
 }
